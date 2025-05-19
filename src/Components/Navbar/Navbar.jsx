b2db035af82ec96f8fdcf53b577485992ba1f5f3
Your Name <you@example.com>
2025-05-20 00:05:17 +0600

    Refactor code structure and remove redundant functions for improved readability and maintainability

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
 
+import { Link } from "react-router";
+import Logo from "../../assets/green-connect.png";
+import ThemeToggle from "../Theme/ThemeToggle";
 const Navbar = () => {
-    return (
-        <div>
-                this is navbar
+  return (
+    <nav className="navbar justify-between  bg-base-100 max-w-7xl mx-auto sticky top-0 z-10 shadow-sm">
+      <Link to="/" className="flex items-center">
+        <div className="w-16 rounded-full">
+          <img alt="green connect logo" src={Logo} className="bg-transparent" />
         </div>
-    );
+        <p className="font-bold text-xl">Green Connect</p>
+      </Link>
+      <div className="">
+        <ul className="hidden md:flex gap-6">
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+        </ul>
+      </div>
+      <ThemeToggle/>
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            
+            <div className="w-10 rounded-full">
+              <img alt="USER PHOTO" src="" />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-64 p-2 shadow space-y-4"
+          >
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+          </ul>
+        </div> 
+        {/* <div className="">
+          <Link
+            className="bg-primary px-4 py-2 rounded-xl text-white font-bold"
+            to="/login"
+          >
+            Log In
+          </Link>
+        </div> */}
+
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import ErrorImg from "../../assets/error.svg";
+import { useNavigate } from "react-router";
 
 const Error = () => {
-    return (
-        <div>
-                this is error page
-        </div>
-    );
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-center p-6 bg-gray-100">
+      <img src={ErrorImg} alt="Error" className="w-64 h-64 md:w-80 md:h-80  mb-6" />
+      <h1 className="text-4xl font-bold text-primary mb-4">
+        Oops! Something Went Wrong
+      </h1>
+      <p className="text-gray-600 mb-6">
+        The page you are looking for might have been removed, had its name
+        changed, or is temporarily unavailable.
+      </p>
+      <button
+        onClick={() => navigate("/")}
+        className="bg-primary cursor-pointer text-white px-6 py-2 rounded-md"
+      >
+        Go Home
+      </button>
+    </div>
+  );
 };
 
 export default Error;
