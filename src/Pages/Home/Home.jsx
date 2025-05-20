76c4fed28234cdd0cf4d3ed9e826020003967b1b
Your Name <you@example.com>
2025-05-20 20:32:25 +0600

    Add new slider image (slider-3.jpeg) to assets

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,20 +1,14 @@
 import React, { use } from 'react';
 import GreenContext from '../../Context/GreenContext';
 import Loader from '../../Components/Loader/Loader';
+import Hero from '../../Components/Hero/Hero';
 
 const Home = () => {
     const {user}=use(GreenContext)
 
     return (
         <div>
-            this is home page {name}
-            <button className='btn btn-primary'>Home</button>
-            <p className="text-primary">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique magni, explicabo fugit cum debitis architecto ratione, doloribus facere distinctio nisi consequatur velit nobis cupiditate vero, rerum voluptates accusantium suscipit aspernatur?
-            </p>
-            <p className="text-secondary">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique magni, explicabo fugit cum debitis architecto ratione, doloribus facere distinctio nisi consequatur velit nobis cupiditate vero, rerum voluptates accusantium suscipit aspernatur?
-            </p>
+                <Hero/>
         </div>
     );
 };
