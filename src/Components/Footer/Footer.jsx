c43638144d6faa37a0afffe539680dcf4e64b4a8
Your Name <you@example.com>
2025-06-26 16:47:10 +0600

    Refactor Footer and DashboardHome components; replace anchor tags with Link components for improved routing, enhance layout and styling, and add new statistics and quick actions sections.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,20 +1,26 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Footer = () => {
   return (
-    <footer className="footer sm:footer-horizontal bg-base-300 text-base-content p-10">
+    <footer className="footer sm:footer-horizontal bg-base-300 text-base-content p-6">
       <nav>
         <h6 className="footer-title">Services</h6>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Community</a>
-        <a className="link link-hover">Idea</a>
+        <Link to={"/"} className="link link-hover">
+          Home
+        </Link>
+        <Link to={"/exploreGardener"} className="link link-hover">
+          exploreGardener
+        </Link>
+        <Link to={"/browseTips"} className="link link-hover">
+          Brows Tips
+        </Link>
       </nav>
       <nav>
         <h6 className="footer-title">Terms</h6>
-        <a className="link link-hover">Privecy & Policy</a>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
+        <Link className="link link-hover">Privecy & Policy</Link>
+        <Link className="link link-hover">About us</Link>
+        <Link className="link link-hover">Contact</Link>
       </nav>
       {/* Contact Info Section */}
       <nav>
@@ -25,9 +31,8 @@ const Footer = () => {
       </nav>
       <nav>
         <h6 className="footer-title">Social</h6>
-        <div className="grid grid-flow-col gap-4">
+        <div className="flex gap-4">
           <a href="https://x.com" target="_blank">
-            
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -49,7 +54,7 @@ const Footer = () => {
               <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
             </svg>
           </a>
-          <a href="https://www.facebook.com" target="_blank" >
+          <a href="https://www.facebook.com" target="_blank">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
